0b0e2c78070d0fdc13c923875f50259a34c26d1e
minseung Kim <msk7262@gmail.com>
2025-04-19 15:36:00 +0900

    commit

---
+++ Coding_test/Solution01.java
@@ -0,0 +1,20 @@
+//빈칸 채우기는 이미 완성된 코드 중 빈칸에 알맞은 코드를 입력하는 문제 타입입니다.
+//빈칸을 제외한 기본 코드는 수정할 수 없습니다.
+//빈칸을 채우지 않을 경우, 실행 결과에 에러 메시지가 표시됩니다.
+
+// 출력 예시 
+//3
+//2
+//1
+//Let's go!
+
+import java.util.Scanner;
+
+public class solution01 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
